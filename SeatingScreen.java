86af26ee2198d8a0e5c921a1bcde6034681e95c3
kbennett30 <41123869+kbennett30@users.noreply.github.com>
2018-07-17 16:19:59 -0500

    Add files via upload

--- SeatingScreen.java
+++ SeatingScreen.java
@@ -2,6 +2,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -20,9 +22,10 @@ public class SeatingScreen extends JPanel{
 		  SeatingScreen(){
 		   //Initialize panels
 		    top = new JPanel();
-		    top.setPreferredSize(new Dimension(300, 600));
+		    top.setPreferredSize(new Dimension(900, 50));
 		    top.setBackground(Color.white);
 		    topText = new JLabel("Choose your seats");
+		    topText.setFont(new Font("Courier New", Font.BOLD, 36));
 		    top.add(topText);
 		    
 		    screen = new JPanel();
@@ -34,42 +37,59 @@ public class SeatingScreen extends JPanel{
 		    screen.setBackground(Color.blue);
 		    
 		    seats = new JPanel();
-		    top.setPreferredSize(new Dimension(400, 400));
-		    top.setBackground(Color.black);
+		    seats.setPreferredSize(new Dimension(400, 400));
+		    seats.setBackground(Color.red);
 		    GridLayout seatsgrid = new GridLayout(6,6);
 		    seats.setLayout(seatsgrid);  //extra row and column to display row and column labels i.e. rowA seat1
 		    
 		    //Adding labels to row numbers
-		    char letter = 'E';
-		    String strletter;
-		    for(int i=1; i<=6; i++){
-		    		strletter = Character.toString(letter);
-		      		JLabel label = new JLabel(strletter); 
-		      		seats.add(label,i,0);
-		      		letter--;
-		    }
-		    
-		    //Adding numbers
-		    for(int j=1; j<= 6; j++){
-		      		String temp = Integer.toString(j);
-		            JLabel label = new JLabel(temp);	
-		      		seats.add(label,0,j);
-		    }
-		        
-		    //Adding checkboxes to the grid
-		    for(int m = 1; m < 6; m++) {
-		   		for(int n = 1; n < 6; n++) {
-		   			//check database here for if seat is taken
-		   			JCheckBox box = new JCheckBox();
-		   			seats.add(box,m,n);
+		   int j = 1;;
+		   char letter = 'F';
+		   String strletter;
+		   //Adding checkboxes to the grid
+		   for(int m = 0; m < 6; m++) {
+		   		for(int n = 0; n < 6; n++) {
+		   			if(m==0) {
+		   				if(n!=0) {
+				   				//add labels for numbers
+					      		String temp = Integer.toString(j);
+						           JLabel label = new JLabel(temp);	
+						      		seats.add(label);
+						      		j++;
+				   				}
+		   				}
+		   			else if(n==0) {
+		   				if(letter == 'F') {
+		   					//add blank label to fill
+				      		JLabel label = new JLabel(""); 
+				      		seats.add(label);
+				      		letter--;
+		   				}
+		   				else {
+		   				//add labels for letters
+			    		strletter = Character.toString(letter);
+			      		JLabel label = new JLabel(strletter); 
+			      		seats.add(label);
+			      		letter--;
+		   			}
+		   			}
+		   			else {
+		   				//add check boxes
+			   			//check database here for if seat is taken
+			   			JCheckBox box = new JCheckBox();
+			   			seats.add(box);
+		   			}
 		   		}
-		    }  
+		    }
+		   //add final label
+  		JLabel label = new JLabel("A"); 
+  		seats.add(label);
 		    
 			//Background
 			JPanel controls = new JPanel();
 			BoxLayout layout = new BoxLayout(controls, BoxLayout.Y_AXIS);
 			controls.setLayout(layout);
-			setPreferredSize(new Dimension(1000, 1050));
+			setPreferredSize(new Dimension(1000, 1000));
 			setBackground(Color.black);
 			
 			//Adding all panels to the screen
